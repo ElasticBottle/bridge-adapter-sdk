66391d2b1ae4631cd6c96a3b1beddf0457b6f81f
ElasticBottle <winstonyeo99@yahoo.com>
2023-07-14 02:46:46 -0400

    chore(core-bridge-adapter-sdk): export Token type

--- packages/elasticbottle-core-bridge-adapter-sdk/src/index.ts
+++ packages/elasticbottle-core-bridge-adapter-sdk/src/index.ts
@@ -1,4 +1,4 @@
 export { CHAIN_NAMES as SupportedChainNames } from "./constants/ChainNames";
 export { BridgeAdapterSdk } from "./lib/BridgeSdk";
 export type { ChainName, ChainSourceAndTarget } from "./types/Chain";
-export type { TokenWithAmount } from "./types/Token";
+export type { Token, TokenWithAmount } from "./types/Token";
