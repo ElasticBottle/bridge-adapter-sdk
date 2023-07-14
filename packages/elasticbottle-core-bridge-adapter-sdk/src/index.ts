2530b8e40f2e778400737c297e7abefcfec10600
ElasticBottle <winstonyeo99@yahoo.com>
2023-07-14 02:08:59 -0400

    chore(core-react-bridge-adapter-sdk): export BridgeAdapterSdk

--- packages/elasticbottle-core-bridge-adapter-sdk/src/index.ts
+++ packages/elasticbottle-core-bridge-adapter-sdk/src/index.ts
@@ -1,3 +1,4 @@
 export { CHAIN_NAMES as SupportedChainNames } from "./constants/ChainNames";
+export { BridgeAdapterSdk } from "./lib/BridgeSdk";
 export type { ChainName, ChainSourceAndTarget } from "./types/Chain";
 export type { TokenWithAmount } from "./types/Token";
