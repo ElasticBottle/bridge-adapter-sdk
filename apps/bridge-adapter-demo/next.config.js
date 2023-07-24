dace15e5a64c6e5057caf1621c92e6fa6b99714e
Tim Hagn <mail@timhagn.com>
2023-07-24 19:25:26 +0200

    fix wagmi build errors by adding pino-pretty, lokijs & encoding devDeps

--- apps/bridge-adapter-demo/next.config.js
+++ apps/bridge-adapter-demo/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@elasticbottle/react-bridge-adapter-sdk"],
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
