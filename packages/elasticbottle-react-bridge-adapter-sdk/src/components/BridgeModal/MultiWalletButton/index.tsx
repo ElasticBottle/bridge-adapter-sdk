88e80959fa6d6e183a4ad55a153744381a0ed3e3
Tim Hagn <mail@timhagn.com>
2023-07-28 17:34:47 +0200

    feat(react-bridge-adapter-sdk): add EVM disconnect button

---
+++ packages/elasticbottle-react-bridge-adapter-sdk/src/components/BridgeModal/MultiWalletButton/index.tsx
@@ -0,0 +1,72 @@
+import { useAccount, useDisconnect } from "wagmi";
+import { Button } from "../../ui/button";
+import {
+  clearChain,
+  useBridgeModalStore,
+} from "../../../providers/BridgeModalContext";
+import { useSolanaWalletMultiButton } from "../WalletSelection/useSolanaWalletMultiButton";
+
+export function MultiWalletButton() {
+  const { sourceChain, targetChain } = useBridgeModalStore.use.chain();
+  const { buttonState, onDisconnect } = useSolanaWalletMultiButton();
+  const { address, connector, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
+
+  const disconnectSolana =
+    (sourceChain === "Solana" || targetChain === "Solana") &&
+    buttonState === "connected";
+  const disconnectEvm =
+    (sourceChain === "Ethereum" || targetChain === "Ethereum") && isConnected;
+  return (
+    <>
+      {disconnectSolana && (
+        <Button
+          variant="ghost"
+          onClick={() => {
+            // TODO: refactor to own callback
+            if (onDisconnect) {
+              if (sourceChain === "Solana") {
+                clearChain("source");
+                useBridgeModalStore.setState((state) => {
+                  state.chain.sourceChain = "Select a chain";
+                });
+              }
+              if (targetChain === "Solana") {
+                clearChain("target");
+                useBridgeModalStore.setState((state) => {
+                  state.chain.targetChain = "Select a chain";
+                });
+              }
+              onDisconnect();
+            }
+          }}
+        >
+          Disconnect
+        </Button>
+      )}
+      {disconnectEvm && (
+        <Button
+          variant="ghost"
+          onClick={() => {
+            // TODO: refactor to own callback
+            if (sourceChain === "Ethereum") {
+              clearChain("source");
+              useBridgeModalStore.setState((state) => {
+                state.chain.sourceChain = "Select a chain";
+              });
+            }
+            if (targetChain === "Ethereum") {
+              clearChain("target");
+              useBridgeModalStore.setState((state) => {
+                state.chain.targetChain = "Select a chain";
+              });
+            }
+            disconnect();
+          }}
+        >
+          Disconnect EVM
+        </Button>
+      )}
+    </>
+  );
+}
