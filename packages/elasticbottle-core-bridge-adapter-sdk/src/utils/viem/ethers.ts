0734ce245b893ec09b4cfb4a9a9dfbfc4363b187
ElasticBottle <winstonyeo99@yahoo.com>
2023-08-13 21:42:51 -0400

    hack(react-bridge-adapter-sdk): eslint disable

--- packages/elasticbottle-core-bridge-adapter-sdk/src/utils/viem/ethers.ts
+++ packages/elasticbottle-core-bridge-adapter-sdk/src/utils/viem/ethers.ts
@@ -41,5 +41,7 @@ export function publicClientToProvider(publicClient: PublicClient) {
         ({ value }) => new providers.JsonRpcProvider(value?.url, network)
       )
     );
+  // TODO: Fix typing here
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   return new providers.JsonRpcProvider(transport.url, network);
 }
