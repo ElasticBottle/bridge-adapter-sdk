29747f47e6952fe73dc8a0a45557892628cc43f3
ElasticBottle <winstonyeo99@yahoo.com>
2023-08-13 20:33:52 -0400

    feat(react-bridge-adapter-sdk): update review swap details page with more info

--- packages/elasticbottle-react-bridge-adapter-sdk/src/components/BridgeModal/SwapReview/index.tsx
+++ packages/elasticbottle-react-bridge-adapter-sdk/src/components/BridgeModal/SwapReview/index.tsx
@@ -1,9 +1,16 @@
+import { Clock2, DollarSign, Droplet } from "lucide-react";
+import { formatSwapFee, formatTime } from "../../../lib/utils";
 import { useBridgeModalStore } from "../../../providers/BridgeModalContext";
 import { Card, CardContent, CardHeader } from "../../ui/card";
 import { PendingTransactionButton } from "../PendingTransaction/PendingTransactionButton";
 
 export function SwapReview() {
-  const { sourceToken, targetToken } = useBridgeModalStore.use.token();
+  const swapInformation = useBridgeModalStore.use.swapInformation?.();
+  if (!swapInformation) {
+    throw new Error("Swap information is not set");
+  }
+
+  const { sourceToken, targetToken } = swapInformation;
   return (
     <div className="bsa-h-full bsa-flex-col bsa-justify-between">
       <Card>
@@ -20,13 +27,32 @@ export function SwapReview() {
             <div className="bsa-flex bsa-items-center bsa-justify-between">
               <div className="bsa-text-muted-foreground">Receiving Token</div>
               <div>
-                {targetToken.selectedAmountFormatted} {targetToken.symbol} (
+                {targetToken.expectedOutputFormatted} {targetToken.symbol} (
                 {targetToken.chain})
               </div>
             </div>
-            <div className="bsa-flex bsa-items-center bsa-justify-between">
-              <div className="bsa-text-muted-foreground">Price Impact</div>
-              <div>0.00%</div>
+            <div className="bsa-flex bsa-items-center bsa-justify-between bsa-text-muted-foreground">
+              <div className="bsa-flex bsa-items-center bsa-justify-start bsa-space-x-2">
+                <Droplet />
+                <div>Price Impact</div>
+              </div>
+              <div>{swapInformation.tradeDetails.priceImpact}%</div>
+            </div>
+            <div className="bsa-flex bsa-items-center bsa-justify-between bsa-text-muted-foreground">
+              <div className="bsa-flex bsa-items-center bsa-justify-start bsa-space-x-2">
+                <Clock2 />
+                <div>Estimated Bridge Time</div>
+              </div>
+              <div>
+                {formatTime(swapInformation.tradeDetails.estimatedTimeMinutes)}
+              </div>
+            </div>
+            <div className="bsa-flex bsa-items-center bsa-justify-between bsa-text-muted-foreground">
+              <div className="bsa-flex bsa-items-center bsa-justify-start bsa-space-x-2">
+                <DollarSign />
+                <div>Fees</div>
+              </div>
+              <div>{formatSwapFee(swapInformation.tradeDetails.fee)}</div>
             </div>
           </div>
         </CardContent>
